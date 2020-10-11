eb46cb30a92f898fc59c886651be1677ada5cd62
Samuel Rowe <samuelrowe1999@gmail.com>
2020-10-11 09:49:57 +0530

    Implemented the invoice manager.
     * Moved the invoice number generator utilities from the hubble-server repository to here.
     * Added Redis as a dependency.
     * An invoice is generated when a "new" subscription is updated. Invoices are now persisted to the database.
     * Updated the `Invoice` model.

---
+++ app/invoice-manager.js
@@ -0,0 +1,117 @@
+const addDays = require("date-fns/addDays");
+const Invoice = require("./model/invoice");
+const redisClient = require("./util/redis");
+
+function pad(number, size, base) {
+    let result = number.toString(base).toUpperCase();
+    while (result.length < size) {
+        result = "0" + result;
+    }
+    return result;
+}
+
+function getInvoiceNumber(prefix, id) {
+    return new Promise((resolve, reject) => {
+        redisClient.incr(id, (error, count) => {
+            if (error) {
+                return reject(error);
+            }
+
+            if (count == 1) {
+                const now = new Date();
+                const midnight = new Date(
+                    now.getFullYear(),
+                    now.getMonth(),
+                    now.getDate(),
+                    0,
+                    0,
+                    0
+                );
+                const sinceMidnight = parseInt(
+                    Math.round((now.getTime() - midnight.getTime()) / 1000)
+                );
+                const ttl = 86400 - sinceMidnight;
+                redisClient.expire(id, ttl);
+            }
+
+            const today = new Date();
+            const date =
+                today.getFullYear() +
+                pad(today.getMonth(), 2, 10) +
+                pad(today.getDate(), 2, 10);
+            const number = pad(count, 4, 36);
+            resolve(`${prefix}-${date}-${number}`);
+        });
+    });
+}
+
+// TODO: Move this to `invoices.test.js`.
+// function generate(prefix, id) {
+//     getInvoiceNumber(prefix, id)
+//         .then((value) => {
+//             console.log(`New invoice number: ${value}`);
+//         })
+//         .catch((error) => {
+//             console.log(error);
+//         });
+// }
+
+// generate("HUB", "hubble_abc");
+// generate("DAR", "darwin_123");
+// generate("DAR", "darwin_123");
+// generate("DAR", "darwin_123");
+// generate("DAR", "darwin_123");
+// generate("HUB", "hubble_abc");
+// generate("HUB", "hubble_abc");
+
+async function create(subscription, items) {
+    let total = 0;
+    let subtotal = 0;
+    for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        total += item.total;
+        subtotal += item.subtotal;
+    }
+
+    const invoice = new Invoice({
+        ownerId: subscription.ownerId,
+        accountId: subscription.accountId,
+        subscriptionId: subscription._id,
+        invoiceNumber: await getInvoiceNumber(
+            "HUB",
+            subscription.ownerId.toString()
+        ),
+        status: "pending",
+        subtotal,
+        total,
+        origin: "purchase",
+        notes: subscription.notes,
+        termsAndConditions: subscription.termsAndConditions,
+        dueAt: addDays(new Date(), 1),
+        closedAt: null,
+        items,
+    });
+
+    return invoice;
+}
+
+async function createInitial(subscription) {
+    const invoice = await create(subscription, [
+        {
+            referenceId: subscription._id,
+            type: "setup_fee",
+            description: "Setup Fee",
+            quantity: 1,
+            startedAt: subscription.createdAt,
+            endedAt: subscription.createdAt,
+            subtotal: subscription.setupFee,
+            total: subscription.setupFee,
+        },
+    ]);
+    return invoice;
+}
+
+module.exports = {
+    create,
+    createInitial,
+};
